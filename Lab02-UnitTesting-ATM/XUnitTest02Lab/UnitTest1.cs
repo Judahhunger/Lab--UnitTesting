b043e514334f499e3d2fbc8a34602af6ff411be3
Judah Hunger <judah.hunger@gmail.com>
2018-06-06 21:55:02 -0700

    Need to refactor and add try catch finally.

---
+++ Lab02-UnitTesting-ATM/XUnitTest02Lab/UnitTest1.cs
@@ -0,0 +1,31 @@
+using System;
+using Xunit;
+using Lab02_UnitTesting_ATM;
+
+
+namespace XUnitTest02Lab
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(500, 100, 400)]
+        [InlineData(500, 600, 500)]
+        public void WithdrawTest(double valueOne, double valueTwo, double valueReturned)
+        {
+            Assert.Equal(valueReturned, Program.WidthdrawMony(valueOne, valueTwo));
+        }
+    }
+
+    public class UnitTest2
+    {
+        [Theory]
+        [InlineData(100, 200, 300)]
+        [InlineData(130, 0, 130)]
+        public void DepositTest(double valueOne, double valueTwo, double valueReturned)
+        {
+            Assert.Equal(valueReturned, Program.DepositMoney(valueOne, valueTwo));
+        }
+    }
+
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Lab--UnitTesting
+
+# Lab 02: Unit Testing
+> makeshift ATM type cosole app.
+
+This project is to make an app that acts like an ATM. 
+
+Where you can add aka deposit money, subtract aka withdraw money, and view account balance with updated balance.
+
+Also to practice Unit Tests and to implement try, catch, finally.
+
+# Examples
+
+current balance is $100, withdraw of $50 => new current balance is $50;
+
+current balance is $200, deposit of $100 => new current balance is $300;
+
